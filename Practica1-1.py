93806abba1717e3558f0abd6aa4e62716dd57697
IDGS801-19002046 <19002046@ualumnos.utleon.edu.mx>
2024-01-17 22:57:31 -0600

    CMRT 17/01/2024

---
+++ Practica1-1.py
@@ -0,0 +1,19 @@
+class piramide:
+    num=0
+
+    def impr(self, num):
+        i = 1
+        while i <= num:
+            print('*' * i)
+            i += 1
+
+def main():
+    obj = piramide()
+    num = int(input("Ingresar un numero entero para la piramide: "))
+    obj.impr(num)
+
+
+if __name__ == "_main_":
+    main()
+    
+    
